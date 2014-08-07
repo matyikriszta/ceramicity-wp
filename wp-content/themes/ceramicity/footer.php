ebdcf6fa796c6bd9ed55d39331b5923d47da6322
Kriszta <matyi.kriszta@gmail.com>
2014-08-07 23:56:39 +0100

    adding semantic fluid grid

--- wp-content/themes/ceramicity/footer.php
+++ wp-content/themes/ceramicity/footer.php
@@ -4,11 +4,5 @@
         <p>Theme by Krisztina Matyi</p>
       </footer>
     </div>
-
-    <script src="js/jquery-1.11.1.min.js"></script>
-    <script src="js/masonry.pkgd.min.js"></script>
-    <script src="js/main.js"></script>
-
-
   </body>
 </html>
