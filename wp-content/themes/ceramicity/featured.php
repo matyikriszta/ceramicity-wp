b69cb81a4df0ec396a02d94cf9e4364b3498bd4e
Kriszta <matyi.kriszta@gmail.com>
2014-08-18 22:21:25 +0100

    adding featured image as background

--- wp-content/themes/ceramicity/featured.php
+++ wp-content/themes/ceramicity/featured.php
@@ -1,7 +1,17 @@
-<ul>
+<div class="featured-post">
   <?php $the_query = new WP_Query( 'showposts=1' ); ?>
   <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-  <li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
-  <li><?php the_content(__('(more…)')); ?></li>
+    <?php if (has_post_thumbnail( $post->ID ) ): ?>
+      <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+      <a href="<?php the_permalink() ?>">
+        <div class="featured-post-image" style="background-image: url('<?php echo $image[0]; ?>')"></div>
+      </a>
+    <?php endif; ?>
+    <div class="featured-post-content">
+      <ul class="grey"><?php the_category(', '); ?></ul>
+      <a href="<?php the_permalink() ?>"><h1><?php the_title(); ?></h1></a>
+      <p><?php the_excerpt(); ?></p>
+      <p class="grey"><?php the_date(); ?> / Leave a comment</p>
+    </div>
   <?php endwhile;?>
-</ul>
+</div>
