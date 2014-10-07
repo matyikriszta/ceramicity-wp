7ed11ca5382144d9e1fd6066e72922f1c822e56d
Kriszta <matyi.kriszta@gmail.com>
2014-10-07 21:44:32 +0100

    adding _archive.scss file, styling archive page

--- wp-content/themes/ceramicity/archive.php
+++ wp-content/themes/ceramicity/archive.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
-  <div id="container"> 
-
-    
+  <div class="archives-title">
     <h1>
       <?php if ( is_day() ) : ?>
-          <?php printf( __( 'Daily Archives: <span>%s</span>', 'twentyten' ), get_the_date() ); ?>
+          <?php printf( __( 'Daily Archives: <span>%s</span>', 'ceramicity' ), get_the_date() ); ?>
       <?php elseif ( is_month() ) : ?>
-          <?php printf( __( 'Monthly Archives: <span>%s</span>', 'twentyten' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'twentyten' ) ) ); ?>
+          <?php printf( __( 'Monthly Archives: <span>%s</span>', 'ceramicity' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'ceramicity' ) ) ); ?>
       <?php elseif ( is_year() ) : ?>
-          <?php printf( __( 'Yearly Archives: <span>%s</span>', 'twentyten' ), get_the_date( _x( 'Y', 'yearly archives date format', 'twentyten' ) ) ); ?>
+          <?php printf( __( 'Yearly Archives: <span>%s</span>', 'ceramicity' ), get_the_date( _x( 'Y', 'yearly archives date format', 'ceramicity' ) ) ); ?>
       <?php else : ?>
-          <?php _e( 'Blog Archives', 'twentyten' ); ?>
+          <?php _e( 'Blog Archives', 'ceramicity' ); ?>
       <?php endif; ?>
-    </h1>
+     </h1>
+  </div>
+  <div id="container"> 
 
       <?php while ( have_posts() ) : the_post(); ?>
         <?php get_template_part('masonry'); ?>
