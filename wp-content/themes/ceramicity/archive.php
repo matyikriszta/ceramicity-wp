30ee8efc61eaa5c29c9a5c009d01fcd3859f1d41
Kriszta <matyi.kriszta@gmail.com>
2014-10-07 21:34:10 +0100

    archive page displaying posts by month

--- wp-content/themes/ceramicity/archive.php
+++ wp-content/themes/ceramicity/archive.php
@@ -1,9 +1,23 @@
 <?php get_header(); ?>
-  <div id="container">
-    <h1>Montly archives</h1>
-          <?php query_posts('offset=1'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <?php get_template_part('masonry'); ?>
-          <?php endwhile; endif; ?>    
+  <div id="container"> 
+
+    
+    <h1>
+      <?php if ( is_day() ) : ?>
+          <?php printf( __( 'Daily Archives: <span>%s</span>', 'twentyten' ), get_the_date() ); ?>
+      <?php elseif ( is_month() ) : ?>
+          <?php printf( __( 'Monthly Archives: <span>%s</span>', 'twentyten' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'twentyten' ) ) ); ?>
+      <?php elseif ( is_year() ) : ?>
+          <?php printf( __( 'Yearly Archives: <span>%s</span>', 'twentyten' ), get_the_date( _x( 'Y', 'yearly archives date format', 'twentyten' ) ) ); ?>
+      <?php else : ?>
+          <?php _e( 'Blog Archives', 'twentyten' ); ?>
+      <?php endif; ?>
+    </h1>
+
+      <?php while ( have_posts() ) : the_post(); ?>
+        <?php get_template_part('masonry'); ?>
+      <?php endwhile; ?>
+
   </div>
 
 <?php get_footer(); ?>
