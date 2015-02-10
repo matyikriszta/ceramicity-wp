767a5b3751f7c461c459a88db9882feee909c0d9
Kriszta <matyi.kriszta@gmail.com>
2015-02-10 23:28:51 +0000

    restructuring single post html

--- wp-content/themes/ceramicity/single.php
+++ wp-content/themes/ceramicity/single.php
@@ -2,40 +2,38 @@
 <?php the_post(); ?>
 
       <div class="divider"></div>
-      <div id="post-aside">
-        <div id="post-nav">
-          <p><?php previous_post_link('%link', '<< Previous'); ?> / <?php next_post_link('%link', 'Next >>'); ?></p>
-          <ul>
-            <li><div class="email"></div><p>Email</p></li>
-            <li><div class="facebook"></div><p>Facebook</p></li>
-            <li><div class="twitter"></div><p>Twitter</p></li>
-            <li><div class="google"></div><p>Google +</p></li>
-            <li><div class="tumblr"></div><p>Tumblr</p></li>
-            <li><div class="pinterest"></div><p>Pinterest</p></li>
-            <li><div class="linkedin"></div><p>LinkedIn</p></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-      </div>
-      <div id="post">
-        <div id="post-title">
+      <div class="single-post">
+        <div class="single-post-title">
           <h1><?php the_title(); ?></h1>
-          <div id="post-meta"> 
+          <div class="single-post-meta"> 
             <?php 
               $archive_year  = get_the_time('Y'); 
               $archive_month = get_the_time('m'); 
               $archive_day   = get_the_time('d'); 
             ?> 
-            <p><span class="grey">by</span> <?php the_author_posts_link(); ?> <span class="grey">/</span> <a href="<?php echo get_day_link( $archive_year, $archive_month, $archive_day); ?>"><?php the_date(); ?></a> <span class="grey">/</span> <?php the_category(', '); ?> <span class="grey">/</span> <a href="#respond">Leave a comment</a></p>
+            <p>by <?php the_author_posts_link(); ?> <span class="grey">/</span> <a href="<?php echo get_day_link( $archive_year, $archive_month, $archive_day); ?>"><?php the_date(); ?></a> <span class="grey">/</span> <?php the_category(', '); ?> <span class="grey">/</span> <a href="#respond">Leave a comment</a></p>
           </div>
         </div>
-        <div id="post-content">
+        <div class="post-aside">
+          <div class="post-nav">
+            <p><?php previous_post_link('%link', '&laquo; Previous'); ?> <span class="grey">/</span> <?php next_post_link('%link', 'Next &raquo;'); ?></p>
+            <ul>
+              <li><div class="email"></div><p>Email</p></li>
+              <li><div class="facebook"></div><p>Facebook</p></li>
+              <li><div class="twitter"></div><p>Twitter</p></li>
+              <li><div class="google"></div><p>Google +</p></li>
+              <li><div class="tumblr"></div><p>Tumblr</p></li>
+              <li><div class="pinterest"></div><p>Pinterest</p></li>
+              <li><div class="linkedin"></div><p>LinkedIn</p></li>
+            </ul>
+          </div>
+        </div>
+        <div class="post-content">
           <?php the_content(); ?>
-          <p>Tags: <?php the_tags('', ', '); ?></p>
-          <div id="respond">
+          <div class="post-tags">
+            Tags: <?php the_tags('', ', '); ?>
+          </div>
+          <div class="post-comment">
             <?php comments_template(); ?>
           </div>
         </div>
